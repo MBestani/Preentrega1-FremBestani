5ee19e5c15faa054494fd1c430c600ce337ac712
MBestani <107009743+MBestani@users.noreply.github.com>
2022-11-29 20:21:16 -0300

    Update index.jsx

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -8,30 +8,33 @@ import { useState } from "react";
 
 
 const Cart = () => {
+    const [isId, setIsId] = useState('')
     const [dataForm, setDataForm] = useState({
         name: '',
         phone: '',
         email: '',
-        address:'', 
+        address: '',
     });
     const { cart, totalPrice, clearCart } = useCartContext();
- const emitirCompra = async (e) => {
-    e.preventDefault ()
-    const order = {
-        
-        buyer: {
-            name: dataForm.name,
-            email: dataForm.email,
-            phone: dataForm.phone,
-            address: dataForm.address,
-        },
-        items: cart.map(product => ({ id: product.id, title: product.title, price: product.price, quantity: product.quantity })),
-        total: totalPrice(),
-    }
-   
+    const emitirCompra = async (e) => {
+        e.preventDefault()
+        const order = {
+
+            buyer: {
+                name: dataForm.name,
+                email: dataForm.email,
+                phone: dataForm.phone,
+                address: dataForm.address,
+            },
+            items: cart.map(product => ({ id: product.id, title: product.title, price: product.price, quantity: product.quantity })),
+            total: totalPrice(),
+        }
+
         const db = getFirestore()
         const orders = collection(db, 'orders')
         addDoc(orders, order)
+            .then(resp => setIsId(resp.id))
+            // .finally(()=> clearCart())
     }
 
     if (cart.length === 0) {
@@ -39,60 +42,66 @@ const Cart = () => {
             <>
                 <p>No hay elementos en el carrito</p>
                 <Link to='/'>Hacer Compras</Link>
+
             </>
         );
     }
 
-   const handleInputChange = (e) => {
-    setDataForm({
-        ...dataForm,
-        [e.target.name]: e.target.value
-    })
-   }
+    const handleInputChange = (e) => {
+        setDataForm({
+            ...dataForm,
+            [e.target.name]: e.target.value
+        })
+    }
 
     return (
         <div>
-            {
-                cart.map(product => <ItemCart key={product.id} product={product} />)
-            }
-            <p>
-                total: ${totalPrice()}
-            </p>
-            <form onSubmit={emitirCompra}>
-                <input 
-                type="text" 
-                name= "name"
-                placeholder="Nombre"
-                onChange={handleInputChange}
-                value={dataForm.name}
-                />
-                <input 
-                type="text" 
-                name= "phone"
-                placeholder="Telefono"
-                value={dataForm.phone}
-                onChange={handleInputChange}
-                />
-                <input 
-                type="text" 
-                name= "email"
-                placeholder="Email"
-                value={dataForm.email}
-                onChange={handleInputChange}
-                />
-                                <input 
-                type="text" 
-                placeholder="Direccion"
-                name="address"
-                value={dataForm.adress}
-                onChange={handleInputChange}
-                />
-                <button type="submit">Emitir Compra</button>
-            </form>
+            <h1>Carrito</h1>
+                {isId && <p>Orden generada con exito: {isId}</p>}
 
-            <button onClick={clearCart}>Vaciar Carrito</button>
-      </div>
+            <>
+                {
+                    cart.map(product => <ItemCart key={product.id} product={product} />)
+                }
+                <p>
+                    total: ${totalPrice()}
+                </p>
+                <form onSubmit={emitirCompra}>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Nombre"
+                        onChange={handleInputChange}
+                        value={dataForm.name}
+                    />
+                    <input
+                        type="text"
+                        name="phone"
+                        placeholder="Telefono"
+                        value={dataForm.phone}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        value={dataForm.email}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Direccion"
+                        name="address"
+                        value={dataForm.adress}
+                        onChange={handleInputChange}
+                    />
+                    <button type="submit">Emitir Compra</button>
+                </form>
 
+                <button onClick={clearCart}>Vaciar Carrito</button>
+            </>
+
+        </div>
     )
 }
 
